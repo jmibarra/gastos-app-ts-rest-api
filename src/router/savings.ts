e1f6cb79f838464631797001a4594ae03f17e8e9
Juan Ibarra <jmibarra86@gmail.com>
2024-10-01 20:36:30 -0300

    Mejoras en un error de la api con metodos cruzados

--- src/router/savings.ts
+++ src/router/savings.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import { isAuthenticated, isSavingOwner } from "../middlewares";
 import { createNewSaving, deleteSaving, getAllPeriodSavings, getAllSavings, getSaving, updateSaving } from "../controllers/savings";
+import { get } from "lodash";
 
 
 export default (router: express.Router) => {
     router.post('/savings', isAuthenticated, createNewSaving);
-    router.get('/savings/:period', isAuthenticated, getAllSavings);
-    router.get('/savings/all/', isAuthenticated, getAllPeriodSavings);
+    router.get('/savings/:period', isAuthenticated, getAllPeriodSavings);
+    router.get('/savings/all/', isAuthenticated, getAllSavings);
     router.get('/savings/:id', isAuthenticated,isSavingOwner, getSaving);
     router.delete('/savings/:id', isAuthenticated, isSavingOwner, deleteSaving);
     router.patch('/savings/:id', isAuthenticated, isSavingOwner, updateSaving);
