58ad2ecb1abaf7e83b9640288e7b058c9f1290bc
Juan Ibarra <jmibarra86@gmail.com>
2024-10-01 20:47:07 -0300

    Cambio las url de ahorros por que entraban en conflictos

--- src/router/savings.ts
+++ src/router/savings.ts
@@ -6,8 +6,8 @@ import { get } from "lodash";
 
 export default (router: express.Router) => {
     router.post('/savings', isAuthenticated, createNewSaving);
-    router.get('/savings/:period', isAuthenticated, getAllPeriodSavings);
-    router.get('/savings/all/', isAuthenticated, getAllSavings);
+    router.get('/savings/period/:period', isAuthenticated, getAllPeriodSavings);
+    router.get('/savings/all', isAuthenticated, getAllSavings);
     router.get('/savings/:id', isAuthenticated,isSavingOwner, getSaving);
     router.delete('/savings/:id', isAuthenticated, isSavingOwner, deleteSaving);
     router.patch('/savings/:id', isAuthenticated, isSavingOwner, updateSaving);
