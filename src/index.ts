d634fedbaefc6e5ff0a32b661dc438c07aecef68
Juan Ibarra <jmibarra86@gmail.com>
2024-10-17 19:28:08 -0300

    Autorizo a todos a llegar a la api

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(
     cors(
         {
-            origin: ['http://localhost:5173','http://localhost:3000',,'http://localhost:8081'],
+            origin: '*',
             credentials: true,
         }
 ));
